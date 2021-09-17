416726cb6a478b174c8513854ae6dbbc2c40a7ca
paulopreto <paulosantiago@usp.br>
2021-09-17 10:37:22 -0300

    v0.2

--- Aula_Cinematica_linear/velocidade_escalar_velocidade_vetorial/posvelacel.m
+++ Aula_Cinematica_linear/velocidade_escalar_velocidade_vetorial/posvelacel.m
@@ -2,21 +2,21 @@ load preto_sprint01.txt
 freq = 60;
 dat = preto_sprint01;
 
+% POSICAO E TEMPO
 frames = dat(:,2);
 posicoes = dat(:,1);
-
 tempos = diff(frames) * 1/freq;
-
 dat1 = [[0;tempos], posicoes]
+temptotal = sum([0;tempos])
 
 % VELOCIDADES
 dist_m = dat1(end, 2)
 dist_km = dist_m / 1000
-velmed_ms = dat1(end, 2) / sum(dat1(:,1))
+velmed_ms = dist_m / temptotal
 velmed_kmh  = velmed_ms * 3.6
 
 velinst_ms = [0; diff(posicoes)] ./ dat1(:, 1)
 velinst_kmh = velinst_ms * 3.6
 
 % ACELERACOES
-[0;diff(velinst_ms)] ./  dat1(:, 1).^2
+acelinst_ms2 = [0;diff(velinst_ms)] ./  dat1(:, 1).^2
