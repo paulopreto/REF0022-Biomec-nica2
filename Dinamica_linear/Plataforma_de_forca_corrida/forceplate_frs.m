af850bdc38f5dcf43efb0ce61620e63fbbc9aae4
paulopreto <paulosantiago@usp.br>
2021-11-03 11:52:32 -0300

    retrope vol 3 corrigido nome do arquivo

--- Dinamica_linear/Plataforma_de_forca_corrida/forceplate_frs.m
+++ Dinamica_linear/Plataforma_de_forca_corrida/forceplate_frs.m
@@ -2,8 +2,8 @@ clc
 close all
 
 pkg load io
-vol1_antepe = dlmread('Voluntario_01_antepe.csv', ',', 5, 0);
-vol1_retropepe = dlmread('Voluntario_01_retrope.csv', ',', 5, 0);
+vol1_antepe = dlmread('Voluntario_03_antepe.csv', ',', 5, 0);
+vol1_retropepe = dlmread('Voluntario_03_retrope.csv', ',', 5, 0);
 #fz1 = vol1_antepe(:, 5); % selecionar a coluna 5 que eh o fz da plataforma de forca 1 
 fz2_vol1_antepe = vol1_antepe(:, 14); % selecionar a coluna 5 que eh o fz da plataforma de forca 2 
 fz2_vol1_retrope = vol1_retropepe(:, 14); % selecionar a coluna 5 que eh o fz da plataforma de forca 2
