7cbf1c3abd7b9b7c2ca5907350ffd432ebb442f8
paulopreto <paulosantiago@usp.br>
2021-11-05 10:23:55 -0300

    AP force & figure

--- Dinamica_linear/Plataforma_de_forca_corrida/forceplate_frs.m
+++ Dinamica_linear/Plataforma_de_forca_corrida/forceplate_frs.m
@@ -5,8 +5,8 @@ pkg load io
 vol1_antepe = dlmread('Voluntario_03_antepe.csv', ',', 5, 0);
 vol1_retropepe = dlmread('Voluntario_03_retrope.csv', ',', 5, 0);
 #fz1 = vol1_antepe(:, 5); % selecionar a coluna 5 que eh o fz da plataforma de forca 1 
-fz2_vol1_antepe = vol1_antepe(:, 14); % selecionar a coluna 5 que eh o fz da plataforma de forca 2 
-fz2_vol1_retrope = vol1_retropepe(:, 14); % selecionar a coluna 5 que eh o fz da plataforma de forca 2
+fz2_vol1_antepe = vol1_antepe(:, 14); % selecionar a coluna 14 que eh o fz da plataforma de forca 2 
+fz2_vol1_retrope = vol1_retropepe(:, 14); % selecionar a coluna 14 que eh o fz da plataforma de forca 2
 
 #plot(fz1, 'b')
 
@@ -25,6 +25,20 @@ figure
 plot(fz2_vol1a, 'r')
 hold on
 plot(fz2_vol1r, 'b')
-title('FRS [Vermelho = antepe; Azul = retrope]')
-ylabel('Force [N]')
+title('Vertical (Fz): FRS [Vermelho = antepe; Azul = retrope]')
+ylabel('Vertical Force [N]')
+xlabel('Frames') % passar para tempo em segundos
+
+
+fy2_vol1_antepe = vol1_antepe(1:8000, 13); % selecionar a coluna 13 que eh o fy da plataforma de forca 2 
+fy2_vol1_retrope = vol1_retropepe(1:8000, 13); % selecionar a coluna 13 que eh o fy da plataforma de forca 2
+fy2_vol1a = fy2_vol1_antepe(fsuporte_a, :);
+fy2_vol1r = fy2_vol1_retrope(fsuporte_r, :);
+
+figure 
+plot(-1*fy2_vol1a, 'r')
+hold on
+plot(-1*fy2_vol1r, 'b')
+title('Anteroposterior (Fy): FRS [Vermelho = antepe; Azul = retrope]')
+ylabel('AP Force [N]')
 xlabel('Frames') % passar para tempo em segundos
