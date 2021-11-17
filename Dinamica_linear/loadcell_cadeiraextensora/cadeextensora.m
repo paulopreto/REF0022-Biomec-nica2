c9350ede02f5d48b80505aef2ea11c30345ca017
paulopreto <paulosantiago@usp.br>
2021-11-17 11:51:43 -0300

    fim da forca em cadeira ext

--- Dinamica_linear/loadcell_cadeiraextensora/cadeextensora.m
+++ Dinamica_linear/loadcell_cadeiraextensora/cadeextensora.m
@@ -7,18 +7,23 @@ data = load(data2import);
 
 
 [valormax, posmax] = max(data);
-posmax = round(posmax)
+posmax = round(posmax);
 
 
 vet_tempo = ([1:length(data)]'  * 1/freq) - 1/freq;
 
 close all
+
+disp(["O tempo para atingir o maximo foi ", num2str(vet_tempo(posmax)), " segundos"])
+disp(["O valor de forca maxima foi de ", num2str(valormax), " Newtons"])
+
+figure()
 plot(vet_tempo, data)
 hold on 
 plot(vet_tempo(posmax), data(posmax), "vr",  "markersize", 8)
-
-disp(["O tempo para atingir o máximo foi ", num2str(vet_tempo(posmax)), " segundos"])
-disp(["O valor de força máxima foi de ", num2str(valormax), " Newtons"])
+xlabel("Time [s]")
+ylabel("Force [N]")
+pause(10)
 
 
 end
