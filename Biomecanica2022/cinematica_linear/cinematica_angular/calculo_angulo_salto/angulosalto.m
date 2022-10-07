8cc784a5857c4bef3f1e399bebe879771b962316
paulopreto <paulosantiago@usp.br>
2022-10-07 15:22:02 -0300

    angulo salto fim

--- Biomecanica2022/cinematica_linear/cinematica_angular/calculo_angulo_salto/angulosalto.m
+++ Biomecanica2022/cinematica_linear/cinematica_angular/calculo_angulo_salto/angulosalto.m
@@ -4,6 +4,7 @@ im = imread(input('Qual a imagem que deseja calcular o angulo?  '));
 imshow(im)
 
 [x, y] = ginput(2)
+res = [x', y'];
 
 VHorizontal = -1 * [1, 0]
 Vsaida = [x(2) - x(1), y(2) - y(1)]
@@ -11,3 +12,5 @@ Vsaida_norm = Vsaida / norm(Vsaida)
 ProdIntVet = dot(VHorizontal, Vsaida_norm)
 AnguloRad = acos(ProdIntVet)
 AnguloGraus = acos(ProdIntVet) * 180/pi
+
+%dlmwrite ("dadossalto.txt", res, "delimiter", ",", "-append")
