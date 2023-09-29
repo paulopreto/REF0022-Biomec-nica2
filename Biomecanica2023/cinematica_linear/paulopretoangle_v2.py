5c26c148e9123b130afc2ef147ed6325db7b3696
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2023-09-29 13:53:10 -0300

    Update paulopretoangle_v2.py

--- Biomecanica2023/cinematica_linear/paulopretoangle_v2.py
+++ Biomecanica2023/cinematica_linear/paulopretoangle_v2.py
@@ -10,7 +10,7 @@ points = []
 lines = []  
 current_frame = 0  
 cap = None  
-video_path = ""  
+video_path = ""
 
 def load_video():
     global cap, current_frame, video_path
@@ -33,99 +33,51 @@ def show_frame():
         return
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
+    # Obter as dimensões originais do vídeo
+    original_width = frame.shape[1]
+    original_height = frame.shape[0]
+
     # Obter as dimensões da tela
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
 
     # Calcular a proporção de redimensionamento
-    original_width = frame.shape[1]
-    original_height = frame.shape[0]
     width_ratio = screen_width / original_width
     height_ratio = screen_height / original_height
     resize_ratio = min(width_ratio, height_ratio)
 
-    # Redimensionar o frame
+    # Redimensionar o frame para que ele se ajuste à tela, mantendo a proporção
     frame = cv2.resize(frame, (int(original_width * resize_ratio), int(original_height * resize_ratio)))
 
     image = Image.fromarray(frame)
     photo = ImageTk.PhotoImage(image=image)
+    canvas.config(width=screen_width, height=screen_height)  # Ajusta o tamanho do canvas para o tamanho da tela
     canvas.create_image(0, 0, image=photo, anchor=tk.NW)
     canvas.image = photo
     frame_label.config(text=f"Frame: {current_frame}")
     window.update()
 
-def next_frame():
-    global current_frame
-    current_frame += 1
-    show_frame()
-
-def prev_frame():
-    global current_frame
-    current_frame = max(0, current_frame - 1)
-    show_frame()
-
-def click_event(event):
-    global points, lines, current_frame
-    x, y = event.x, event.y
-    points.append((x, y))
-    if len(points) == 1:
-        canvas.create_oval(x-3, y-3, x+3, y+3, fill='green', tags="marker")
-    elif len(points) == 2:
-        canvas.create_rectangle(x-3, y-3, x+3, y+3, fill='red', tags="marker")
-        canvas.create_line(points, fill='blue', tags="marker")
-        lines.append(points)
-        points = []
-        if len(lines) == 2:
-            angle = calculate_angle()
-            save_data(angle)
-            lines.clear()
-
-def calculate_angle():
-    vec1 = (lines[0][1][0] - lines[0][0][0], lines[0][1][1] - lines[0][0][1])
-    vec2 = (lines[1][1][0] - lines[1][0][0], lines[1][1][1] - lines[1][0][1])
-    dot_product = vec1[0] * vec2[0] + vec1[1] * vec2[1]
-    mag1 = (vec1[0]**2 + vec1[1]**2)**0.5
-    mag2 = (vec2[0]**2 + vec2[1]**2)**0.5
-    cos_theta = dot_product / (mag1 * mag2)
-    angle = degrees(acos(cos_theta))
-    return angle
-
-def save_data(angle):
-    global current_frame, video_path
-    output_filename = video_path.rsplit('.', 1)[0] + "_final_angle.txt"
-    add_header = not os.path.exists(output_filename)
-    with open(output_filename, 'a') as f:
-        if add_header:
-            f.write("Frame,Point1_x,Point1_y,Point2_x,Point2_y,Point3_x,Point3_y,Point4_x,Point4_y,Angle_degrees\n")
-        line_data = f"{current_frame},{lines[0][0][0]},{lines[0][0][1]},{lines[0][1][0]},{lines[0][1][1]},{lines[1][0][0]},{lines[1][0][1]},{lines[1][1][0]},{lines[1][1][1]},{angle:.2f}"
-        f.write(line_data + '\n')
-
 window = tk.Tk()
 window.title("Angle Calculator in Video")
+window.geometry(f"{window.winfo_screenwidth()}x{window.winfo_screenheight()}")  # Define o tamanho da janela para o tamanho da tela
 
-main_frame = Frame(window)
-main_frame.pack(fill=tk.BOTH, expand=1)
-
-canvas_frame = Frame(main_frame)
-canvas_frame.pack(side=tk.LEFT)
-
-canvas = Canvas(canvas_frame, bg="black", width=600, height=400)
-canvas.pack(side=tk.LEFT, pady=20, padx=10)
+canvas = Canvas(window, bg="black")
+canvas.pack(fill=tk.BOTH, expand=True)
 canvas.bind("<Button-1>", click_event)
 
-frame_label = Label(main_frame, text=f"Frame: {current_frame}")
+frame_label = Label(window, text=f"Frame: {current_frame}")
 frame_label.pack(pady=10)
 
-btn_frame = Frame(main_frame)
-btn_frame.pack(side=tk.LEFT, padx=10)
+btn_frame = Frame(window)
+btn_frame.pack(pady=20)
 
 btn_load = Button(btn_frame, text="Load Video", command=load_video)
-btn_load.pack(pady=10)
+btn_load.pack(side=tk.LEFT, padx=10)
 
 btn_prev = Button(btn_frame, text="<<", command=prev_frame)
-btn_prev.pack(pady=10)
+btn_prev.pack(side=tk.LEFT, padx=10)
 
 btn_next = Button(btn_frame, text=">>", command=next_frame)
-btn_next.pack(pady=10)
+btn_next.pack(side=tk.LEFT, padx=10)
 
 window.mainloop()
